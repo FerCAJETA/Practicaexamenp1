7e2173bdad482c25d3ff6fecbad2a3223f6a7927
XFerTz <2402999@utrivieramaya.edu.mx>
2024-10-17 09:20:30 -0500

    Entregable 17/10/24

--- js/script.js
+++ js/script.js
@@ -8,3 +8,12 @@ document.addEventListener('DOMContentLoaded', function () {
 })
 
 document.getElementById('year').textContent = new Date().getFullYear();
+
+// script de contacto
+
+const value = document.querySelector('#range_value')
+const input = document.querySelector('#range')
+value.textContent = input.value;
+input.addEventListener ("input", (event) => {
+    value.textContent = event.target.value;
+});
